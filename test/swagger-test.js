f4f7a9cb7007b482b90080dec8814b6bc3b68fe9
robmckinnon <rob@movingflow>
2017-08-21 11:57:45 +0100

    Fix test of https get swagger
    Change expectation to expect URL object instead of string URI.

--- test/swagger-test.js
+++ test/swagger-test.js
@@ -1,8 +1,10 @@
+/* eslint import/no-extraneous-dependencies: off */
 const assert = require('assert');
 const sinon = require('sinon');
 const swagger = require('../lib/swagger.js');
 const https = require('https');
 const fs = require('fs');
+const { URL } = require('url');
 
 describe('fetchSwagger', () => {
   describe('when SWAGGER env contains URI', () => {
@@ -14,7 +16,8 @@ describe('fetchSwagger', () => {
     it('does HTTP GET of URI', () => {
       sinon.stub(https, 'get').returns({ on: () => {} });
       swagger.fetchSwagger(() => {});
-      assert(https.get.calledWithMatch(uri));
+      const options = new URL(uri);
+      assert(https.get.calledWithMatch(options));
     });
   });
 
