2182d7ab70c926797dcb507e1931118df44bfff6
robmckinnon <rob@movingflow>
2017-09-18 11:26:57 +0100

    Move morgan logging setup to lib/index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 if (!process.env.DEBUG) process.env.DEBUG = 'error,log';
 
 const port = (process.env.PORT || 8001);
-const morgan = require('morgan');
 const log = require('debug')('log');
 const { initApp } = require('./lib');
 
 initApp((app) => {
-  app.use(morgan('dev'));
   app.listen(port, () => {
     log(`running on localhost:${port} ... `);
   });
