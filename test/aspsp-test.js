7205d2296888a47ee36713fd400e49990d05834a
robmckinnon <rob@movingflow>
2017-09-28 15:18:38 +0100

    Add mock directory OBAccountPaymentServiceProviders endpoint #CDRW-664
    Add to test in TPP app OBAccountPaymentServiceProviders identity data retrieval.

---
+++ test/aspsp-test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const request = require('supertest'); // eslint-disable-line
+const { app } = require('../lib/app.js');
+
+describe('/scim/v2/OBAccountPaymentServiceProviders', () => {
+  it('returns JSON payload', (done) => {
+    request(app)
+      .get('/scim/v2/OBAccountPaymentServiceProviders')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.Resources.length, 3);
+        assert.equal(res.body.Resources[0].AuthorisationServers.CustomerFriendlyName, 'AAA Example Bank');
+        assert.equal(res.body.Resources[0].id, 'aaa-example-bank');
+        done();
+      });
+  });
+});
