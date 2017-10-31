c1f54ed4a5f7935db4cd31e6f6b60ccc0caf7288
Ezo Saleh <ezo@the-experimenters.com>
2017-10-31 10:51:52 +0000

    Testing OpenIDConfigEndPointUri is set for an ASPSP.

--- test/aspsp-test.js
+++ test/aspsp-test.js
@@ -1,5 +1,8 @@
 const assert = require('assert');
 const request = require('supertest'); // eslint-disable-line
+const OPENID_CONFIG_ENDPOINT_URL = 'http://localhost';
+
+process.env.OPENID_CONFIG_ENDPOINT_URL = OPENID_CONFIG_ENDPOINT_URL;
 const { app } = require('../lib/app.js');
 
 describe('/scim/v2/OBAccountPaymentServiceProviders', () => {
@@ -8,12 +11,14 @@ describe('/scim/v2/OBAccountPaymentServiceProviders', () => {
       .get('/scim/v2/OBAccountPaymentServiceProviders')
       .set('Accept', 'application/json')
       .end((err, res) => {
+        const authServer = res.body.Resources[0].AuthorisationServers[0];
         assert.equal(res.status, 200);
         assert.equal(res.body.Resources.length, 3);
-        assert.equal(res.body.Resources[0].AuthorisationServers[0].CustomerFriendlyName, 'AAA Example Bank');
         assert.equal(res.body.Resources[0].id, 'aaa-example-bank');
-        assert.equal(res.body.Resources[0].AuthorisationServers[0].BaseApiDNSUri, 'http://aaa-example-bank.example.com');
-        assert.equal(res.body.Resources[0].AuthorisationServers[0].CustomerFriendlyLogoUri, '');
+        assert.equal(authServer.CustomerFriendlyName, 'AAA Example Bank');
+        assert.equal(authServer.BaseApiDNSUri, 'http://aaa-example-bank.example.com');
+        assert.equal(authServer.CustomerFriendlyLogoUri, '');
+        assert.equal(authServer.OpenIDConfigEndPointUri, `${OPENID_CONFIG_ENDPOINT_URL}/${res.body.Resources[0].id}`);
         done();
       });
   });
